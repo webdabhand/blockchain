c8dfde5924f730f9c119702fd1938ce94cb28607
WebDabhands <webdabhands@gmail.com>
2024-07-18 19:54:06 +0530

    changes

--- solana-blockchain/src/app/Build/page.js
+++ solana-blockchain/src/app/Build/page.js
@@ -51,116 +51,119 @@ const page = () => {
             </p>
           </Col>
         </Row>
-        <Row>
-          <Col className="backgroundimg">
+        <Row className="backgroundimg">
+          <Col>
             <div>
               <Image src={backgroundimg} alt="/" />
             </div>
-            <div>
-              <h3>Solana Devlopment Course</h3>
+            <div className="build-image-text">
+              <button>17 Chapters</button>
+              <h1>Solana Devlopment Course</h1>
               <p>
-                Quickstart your Solana development starting from nothing to
-                complex programs.
+                Quickstart your Solana development starting <br />
+                from nothing to complex programs.
               </p>
+              <IoArrowForwardCircleOutline className="circle-arrow" />
             </div>
           </Col>
         </Row>
         <Row className="buildspace-three-background">
-          <Col className="chapter-buildspacce">
-            <div className="one-two">
-              <Image src={buildspace1} alt="/" />
-            </div>
-            <div className="chapter-buildspacce-text">
-              <div>
-                <button>14 Chapters</button>
-                <p>Buildspace</p>
+          <Col>
+            <div className=" chapter-buildspacce">
+              <div className="buildspace1-image">
+                <Image src={buildspace1} alt="/" />
               </div>
-              <div>
-                <IoArrowForwardCircleOutline />
+              <div className="chapter-buildspacce-text">
+                <div>
+                  <button>14 Chapters</button>
+                  <p>Buildspace</p>
+                </div>
+                <div>
+                  <IoArrowForwardCircleOutline className="circle-arrow" />
+                </div>
               </div>
             </div>
           </Col>
-          <Col className="chapter-buildspacce">
-            <div>
+          <Col>
+            <div className=" chapter-buildspacce">
               <Image src={buildspace2} alt="/" />
-            </div>
-            <div className="chapter-buildspacce-text">
-              <div>
-                <button>12 Chapters</button>
-                <p>Solana Bytes</p>
-              </div>
-              <div>
-                <IoArrowForwardCircleOutline />
+              <div className="chapter-buildspacce-text">
+                <div>
+                  <button>12 Chapters</button>
+                  <p>Solana Bytes</p>
+                </div>
+                <div>
+                  <IoArrowForwardCircleOutline className="circle-arrow" />
+                </div>
               </div>
             </div>
           </Col>
-          <Col className="chapter-buildspacce">
-            <div>
+          <Col>
+            <div className="chapter-buildspacce">
               <Image src={buildspace3} alt="/" />
-            </div>
-            <div className="chapter-buildspacce-text">
-              <div>
-                <button>1 Chapters</button>
-                <p> Scaffold Series</p>
-              </div>
-              <div>
-                <IoArrowForwardCircleOutline />
+              <div className="chapter-buildspacce-text">
+                <div>
+                  <button>1 Chapters</button>
+                  <p> Scaffold Series</p>
+                </div>
+                <div>
+                  <IoArrowForwardCircleOutline className="circle-arrow" />
+                </div>
               </div>
             </div>
           </Col>
-        </Row>
-        <Row className="buildspace-three-background background-three ">
-          <Col className="chapter-buildspacce">
-            <div>
+          <Col>
+            <div className="chapter-buildspacce">
               <Image src={buildspace4} alt="/" />
-            </div>
-            <div className="chapter-buildspacce-text">
-              <div>
-                <button>3 Chapters</button>
-                <p>Freecodecamp Solana Course</p>
-              </div>
-              <div>
-                <IoArrowForwardCircleOutline />
+              <div className="chapter-buildspacce-text">
+                <div>
+                  <button>3 Chapters</button>
+                  <p>Freecodecamp Solana Course</p>
+                </div>
+                <div>
+                  <IoArrowForwardCircleOutline className="circle-arrow" />
+                </div>
               </div>
             </div>
           </Col>
-          <Col className="chapter-buildspacce">
-            <div>
+          <Col>
+            <div className="chapter-buildspacce">
               <Image src={buildspace5} alt="/" />
-            </div>
-            <div className="chapter-buildspacce-text ">
-              <div>
-                <button>167 Chapters</button>
-                <p>Solana Devlopment by Knox</p>
-              </div>
-              <div>
-                <IoArrowForwardCircleOutline />
+              <div className="chapter-buildspacce-text ">
+                <div>
+                  <button>167 Chapters</button>
+                  <p>Solana Devlopment by Knox</p>
+                </div>
+                <div>
+                  <IoArrowForwardCircleOutline className="circle-arrow" />
+                </div>
               </div>
             </div>
           </Col>
-          <Col className="chapter-buildspacce">
-            <div>
+          <Col>
+            <div className="chapter-buildspacce">
               <Image src={buildspace6} alt="/" />
-            </div>
-            <div className="chapter-buildspacce-text">
-              <div>
-                <button>8 Chapters</button>
-                <p> solana Bootcamp</p>
-              </div>
-              <div>
-                <IoArrowForwardCircleOutline />
+
+              <div className="chapter-buildspacce-text">
+                <div>
+                  <button>8 Chapters</button>
+                  <p> solana Bootcamp</p>
+                </div>
+                <div>
+                  <IoArrowForwardCircleOutline className="circle-arrow" />
+                </div>
               </div>
             </div>
           </Col>
         </Row>
       </Container>
       <Container>
-        <div>
-          <h1>Dig deeper.</h1>
-          <p>Learn from resources across the greater Solana ecosystem.</p>
-        </div>
-        <Row className="card-title">
-          <Col lg={4}>
+        <Row className="card-title buildspace-three-background">
+          <div>
+            <h1>Dig deeper.</h1>
+            <p>Learn from resources across the greater Solana ecosystem.</p>
+          </div>
+          <Col>
             <div className="card edit-card">
               <div className="card-body">
                 <p className="card-titles">TOOL / LIBRTARY</p>
@@ -170,12 +173,12 @@ const page = () => {
                   SPL tokens, wallets and more.
                 </p>
                 <p href="#" className=" btnn-primary ">
-                  Learn more <GoArrowUpRight />
+                  Learn more <GoArrowUpRight className="arrow-right" />
                 </p>
               </div>
             </div>
           </Col>
-          <Col lg={4}>
+          <Col>
             <div className="card edit-card">
               <div className="card-body">
                 <p className="card-titles">TOOL / LIBRTARY</p>
@@ -186,12 +189,12 @@ const page = () => {
                   Solana.
                 </p>
                 <p href="#" className=" btnn-primary ">
-                  Learn more <GoArrowUpRight />
+                  Learn more <GoArrowUpRight className="arrow-right" />
                 </p>
               </div>
             </div>
           </Col>
-          <Col lg={4} className="cards">
+          <Col className="cards">
             <div className="card edit-card">
               <div className="card-body">
                 <p className="card-titles">TOOL / LIBRTARY</p>
@@ -201,14 +204,14 @@ const page = () => {
                   software users and developers.
                 </p>
                 <p href="#" className=" btnn-primary ">
-                  Learn more <GoArrowUpRight />
+                  Learn more <GoArrowUpRight className="arrow-right" />
                 </p>
               </div>
             </div>
           </Col>
-        </Row>
-        <Row className="card-title">
-          <Col lg={4}>
+          {/* </Row>
+        <Row className="card-title"> */}
+          <Col>
             <div className="card edit-card">
               <div className="card-body">
                 <p className="card-titles">TOOL / LIBRTARY</p>
@@ -218,12 +221,12 @@ const page = () => {
                   SPL tokens, wallets and more.
                 </p>
                 <p href="#" className=" btnn-primary ">
-                  Learn more <GoArrowUpRight />
+                  Learn more <GoArrowUpRight className="arrow-right" />
                 </p>
               </div>
             </div>
           </Col>
-          <Col lg={4}>
+          <Col>
             <div className="card edit-card">
               <div className="card-body">
                 <p className="card-titles">TOOL / LIBRTARY</p>
@@ -233,7 +236,7 @@ const page = () => {
                   SPL tokens, wallets and more.
                 </p>
                 <p href="#" className=" btnn-primary ">
-                  Learn more <GoArrowUpRight />
+                  Learn more <GoArrowUpRight className="arrow-right" />
                 </p>
                 <div className="card-shade">
                   <Image src={shade} alt="/" />
@@ -241,7 +244,7 @@ const page = () => {
               </div>
             </div>
           </Col>
-          <Col lg={4}>
+          <Col>
             <div className="card edit-card">
               <div className="card-body">
                 <p className="card-titles">TOOL / LIBRTARY</p>
@@ -251,14 +254,13 @@ const page = () => {
                   SPL tokens, wallets and more.
                 </p>
                 <p href="#" className=" btnn-primary ">
-                  Learn more <GoArrowUpRight />
+                  Learn more <GoArrowUpRight className="arrow-right" />
                 </p>
               </div>
             </div>
           </Col>
-        </Row>
-        <Row className="card-title">
-          <Col lg={4}>
+
+          <Col>
             <div className="card edit-card">
               <div className="card-body">
                 <p className="card-titles">TOOL / LIBRTARY</p>
@@ -268,12 +270,12 @@ const page = () => {
                   SPL tokens, wallets and more.
                 </p>
                 <p href="#" className=" btnn-primary ">
-                  Learn more <GoArrowUpRight />
+                  Learn more <GoArrowUpRight className="arrow-right" />
                 </p>
               </div>
             </div>
           </Col>
-          <Col lg={4}>
+          <Col>
             <div className="card edit-card">
               <div className="card-body">
                 <p className="card-titles">TOOL / LIBRTARY</p>
@@ -283,26 +285,26 @@ const page = () => {
                   SPL tokens, wallets and more.
                 </p>
                 <p href="#" className="btnn-primary">
-                  Learn more <GoArrowUpRight />
+                  Learn more <GoArrowUpRight className="arrow-right" />
                 </p>
               </div>
             </div>
           </Col>
-          <Col lg={4}></Col>
+          <Col className="card-blank-col"></Col>
         </Row>
       </Container>
       <div className="tools-section">
         <Container>
-          <div className="solana-tools">
-            <h1>Go to the source.</h1>
-            <p>Read the documentation for Solana and popular tools.</p>
-          </div>
-          <Row className="build-docs">
-            <Col lg={4} className="view-all-section">
+          <Row className="build-docs  buildspace-three-background">
+            <div className="solana-tools">
+              <h1>Go to the source.</h1>
+              <p>Read the documentation for Solana and popular tools.</p>
+            </div>
+            <Col className="view-all-section">
               <div className="build-solana-docs">
                 <h1>Solana Docs</h1>
                 <button>
-                  VIEW ALL <GoArrowUpRight />
+                  VIEW ALL <GoArrowUpRight className="arrow-right" />
                 </button>
               </div>
               <div className="learn-line">
@@ -310,11 +312,11 @@ const page = () => {
                 level understanding of Solana's architecture.
               </div>
             </Col>
-            <Col lg={4} className="view-all-section">
+            <Col className="view-all-section">
               <div className="build-solana-docs">
                 <h1>Metaplex Docs</h1>
                 <button>
-                  VIEW ALL <GoArrowUpRight />
+                  VIEW ALL <GoArrowUpRight className="arrow-right" />
                 </button>
               </div>
               <div className="learn-line">
@@ -323,64 +325,69 @@ const page = () => {
               </div>
             </Col>
           </Row>
-          <Row className="change-log">
-            <Col lg={6}>
-              <h1>Solana Changelog</h1>
-              <p>
-                Some more Solana changes from Jacob & Joe. Subscribe to the
-                newsletter: https://solana.us17.list-manage.com/s... Proposal -
-                Priced Compute Units: ...
-              </p>
-              <button>LATEST EPISODE</button>
+          <Row className="change-log  buildspace-three-background">
+            <Col>
+              <div className=" more-changelog">
+                <h1>Solana Changelog</h1>
+                <p>
+                  Some more Solana changes from Jacob & Joe. Subscribe to the
+                  newsletter: https://solana.us17.list-manage.com/s... Proposal
+                  - Priced Compute Units: ...
+                </p>
+                <button>LATEST EPISODE</button>
+              </div>
             </Col>
-
-            <Col lg={6} className="change-log-image">
+            <Col className="change-log-image">
               <Image src={changelog} alt="/" />
             </Col>
           </Row>
-          <Row className="resources">
-            <Col lg={6} className="resources-colum">
-              <h2>Solana Developer Update</h2>
-              <p>
-                Sign up to the newsletter and learn about new resources, new
-                commits, new proposals, and more.
-              </p>
-              <form>
-                <div className="form-group email-edit">
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="Email"
-                  />
-                  <div className="submit-button">
-                    <button type="submit" className="btn btn-primary submit">
-                      SIGN UP
+          <Row className="resources  buildspace-three-background">
+            <Col>
+              <div className="resources-colum">
+                <h2>Solana Developer Update</h2>
+                <p>
+                  Sign up to the newsletter and learn about new resources, new
+                  commits, new proposals, and more.
+                </p>
+                <form>
+                  <div className="form-group email-edit">
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                      placeholder="Email"
+                    />
+                    <div className="submit-button">
+                      <button type="submit" className="btn btn-primary submit">
+                        SIGN UP
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </Col>
+            <Col>
+              <div className=" resources-colum ">
+                <h2>Even more resources</h2>
+                <p>
+                  More videos, more episodes. Discussions between industry
+                  leaders in both blockchain and technology, our team, and
+                  community developers.
+                </p>
+                <div className="social-button">
+                  <div>
+                    <button>
+                      YOUTUBE
+                      <Image src={youtube} alt="/" />
                     </button>
                   </div>
-                </div>
-              </form>
-            </Col>
-            <Col lg={6} className=" resources-colum ">
-              <h2>Even more resources</h2>
-              <p>
-                More videos, more episodes. Discussions between industry leaders
-                in both blockchain and technology, our team, and community
-                developers.
-              </p>
-              <div className="social-button">
-                <div>
-                  <button>
-                    YOUTUBE
-                    <Image src={youtube} alt="/" />
-                  </button>
-                </div>
-                <div>
-                  <button>
-                    PODCAST
-                    <Image src={podcast} alt="/" />
-                  </button>
+                  <div>
+                    <button>
+                      PODCAST
+                      <Image src={podcast} alt="/" />
+                    </button>
+                  </div>
                 </div>
               </div>
             </Col>
